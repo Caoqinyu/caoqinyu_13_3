0906fc32ebdfa2e9b2b0c63b6698950a38d3a690
student <student@imac-05.local>
2018-12-18 13:14:52 +0800

    finish

--- caoqinyu_13_3/ViewController.swift
+++ caoqinyu_13_3/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var notice: UILabel!
+    @IBOutlet weak var tempe: UILabel!
+    @IBOutlet weak var wuran: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var city: UILabel!
+    var cityInfo : [String : Any]?
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(cityInfo)
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        let info1 = cityInfo!["cityInfo"] as! [String : String]
+        city.text = info1["city"]
+        date.text = (cityInfo!["date"] as! String)
+        
+        let info3 = cityInfo!["data"] as! [String : Any]
+        tempe.text = info3["wendu"] as! String
+        wuran.text = info3["quality"] as!String
+       notice.text = info1["cityId"]
 
 
 }
 
+}
